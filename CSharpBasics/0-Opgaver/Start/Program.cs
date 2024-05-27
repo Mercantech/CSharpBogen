62062d0fb6a04b45f05cb1ceb5cbe812bc7f24ae
MAGS <120091142+Mercantec-MAGS@users.noreply.github.com>
2024-05-27 13:32:19 +0200

    Update Program.cs

--- CSharpBasics/0-Opgaver/Start/Program.cs
+++ CSharpBasics/0-Opgaver/Start/Program.cs
@@ -2,9 +2,40 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hej og Velkommen til starter opgaverne!");
+
+            Opgave1();
+            Opgave2();
+            Opgave3();
+            Opgave4();
+        }
+
+        static void header(int opgavernummer) {
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine($"Her starter opgave {opgavernummer}");
+        }
+
+        static void Opgave1()
+        {
+            header(1);
+        }
+
+
+        static void Opgave2()
+        {
+            header(2);
+        }
+        static void Opgave3()
+        {
+            header(3);
+        }
+        static void Opgave4()
+        {
+            header(4);
         }
     }
 }
